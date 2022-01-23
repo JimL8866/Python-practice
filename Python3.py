b517db9c1627cf466eb7abe42243024f81d0efdd
Jim <chengxuyuan9527@gmail.com>
2022-01-24 08:55:26 +1100

    Update Python3.py

--- Python3.py
+++ Python3.py
@@ -1,15 +1,17 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 to_find = int(input("Please put a number\n"))
 
-# for i in range(len(my_list)):
-#     found = my_list[i] == to_find
-#     if found:
-#         break
-#
-# if found:
-#     print("Element found at index", i)
-# else:
-#     print("absent")
+for i in range(len(my_list)):
+    found = my_list[i] == to_find      # found = True
+    if found:
+        break
+
+if found:
+    print("Element found at index", i)
+else:
+    print("absent")
+
+# use index() method
 try:
     foo = my_list.index(to_find)
     if foo:
